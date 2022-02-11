370a015417589308d8c3511e154896d690a442b9
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-02-11 13:14:35 +0300

    Контент через ИБ

---
+++ local/templates/.default/components/bitrix/catalog/content/realweb/catalog.element/.default/template.php
@@ -0,0 +1,29 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+    die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$name = !empty($arResult['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])
+    ? $arResult['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
+    : $arResult['NAME'];
+
+?>
+<? //if ($arResult['PROPERTIES']['IS_CONTAINER']['VALUE'] == 'Y'): ?>
+    <div class="text container">
+        <div class="wide">
+            <?php echo $arResult['DETAIL_TEXT']; ?>
+        </div>
+    </div>
+<? //else: ?>
+    <?php// echo $arResult['DETAIL_TEXT']; ?>
+<?// endif; ?>
