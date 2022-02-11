eab1a0ce06be2382b6bbb5f94563642b60ce40a7
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-02-11 13:05:59 +0300

    Контент через ИБ

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,4 +1,7 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+use \Bitrix\Main\Page\Asset,
+    \Realweb\Site\Site;
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <!DOCTYPE html>
 <html>
 <head>
