929821651959e34b16fa67580fd955e68158f1e9
Varvara Novchenkova <wassa_17@mail.ru>
2022-02-11 10:42:10 +0300

    заливка шаблона

--- index.php
+++ index.php
@@ -1,129 +1,41 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetPageProperty("title", "Демонстрационная версия продукта «1С-Битрикс: Управление сайтом»");
+$APPLICATION->SetPageProperty("title", "Стома");
 $APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
-$APPLICATION->SetTitle("Статьи");
+$APPLICATION->SetTitle("Главная");
 ?>
 
-<!-- -->
-				<?$APPLICATION->IncludeComponent("bitrix:news.list", "articles", Array(
-					"IBLOCK_TYPE"	=>	"articles",
-					"IBLOCK_ID"	=>	"2",
-					"NEWS_COUNT"	=>	"5",
-					"SORT_BY1"	=>	"ACTIVE_FROM",
-					"SORT_ORDER1"	=>	"DESC",
-					"SORT_BY2"	=>	"SORT",
-					"SORT_ORDER2"	=>	"ASC",
-					"FILTER_NAME"	=>	"",
-					"FIELD_CODE"	=>	array(
-					),
-					"PROPERTY_CODE"	=>	array(
-						0	=>	"FORUM_MESSAGE_CNT",
-						1	=>	"rating",
-					),
-					"DETAIL_URL"	=>	"/content/articles/#ELEMENT_ID#/",
-					"CACHE_TYPE"	=>	"A",
-					"CACHE_TIME"	=>	"3600",
-					"CACHE_FILTER"	=>	"N",
-					"PREVIEW_TRUNCATE_LEN"	=>	"0",
-					"ACTIVE_DATE_FORMAT"	=>	"M j, Y, H:m",
-					"DISPLAY_PANEL"	=>	"N",
-					"SET_TITLE"	=>	"N",
-					"INCLUDE_IBLOCK_INTO_CHAIN"	=>	"Y",
-					"ADD_SECTIONS_CHAIN"	=>	"Y",
-					"HIDE_LINK_WHEN_NO_DETAIL"	=>	"N",
-					"PARENT_SECTION"	=>	"",
-					"DISPLAY_TOP_PAGER"	=>	"N",
-					"DISPLAY_BOTTOM_PAGER"	=>	"N",
-					"PAGER_TITLE"	=>	"Статьи",
-					"PAGER_SHOW_ALWAYS"	=>	"N",
-					"PAGER_TEMPLATE"	=>	"",
-					"PAGER_DESC_NUMBERING"	=>	"N",
-					"PAGER_DESC_NUMBERING_CACHE_TIME"	=>	"36000",
-					"PAGER_SHOW_ALL" => "N",
-					"DISPLAY_DATE"	=>	"Y",
-					"DISPLAY_NAME"	=>	"Y",
-					"DISPLAY_PICTURE"	=>	"N",
-					"DISPLAY_PREVIEW_TEXT"	=>	"Y"
-					)
-					);?>
-				<!-- -->
-<!-- --><h1>Новости</h1>
-					<?$APPLICATION->IncludeComponent(
-						"bitrix:news.list",
-						"",
-						Array(
-							"DISPLAY_DATE" => "Y",
-							"DISPLAY_NAME" => "Y",
-							"DISPLAY_PICTURE" => "N",
-							"DISPLAY_PREVIEW_TEXT" => "Y",
-							"IBLOCK_TYPE" => "news",
-							"IBLOCK_ID" => "3",
-							"NEWS_COUNT" => "5",
-							"SORT_BY1" => "ACTIVE_FROM",
-							"SORT_ORDER1" => "DESC",
-							"SORT_BY2" => "SORT",
-							"SORT_ORDER2" => "ASC",
-							"FILTER_NAME" => "",
-							"FIELD_CODE" => Array("",""),
-							"PROPERTY_CODE" => Array("",""),
-							"DETAIL_URL" => "/content/news/#SECTION_ID#/#ELEMENT_ID#/",
-							"PREVIEW_TRUNCATE_LEN" => "0",
-							"ACTIVE_DATE_FORMAT" => "d.m.Y",
-							"DISPLAY_PANEL" => "N",
-							"SET_TITLE" => "N",
-							"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-							"CACHE_TIME" => "3600",
-							"CACHE_FILTER" => "N",
-							"DISPLAY_TOP_PAGER" => "N",
-							"DISPLAY_BOTTOM_PAGER" => "N",
-							"PAGER_TITLE" => "Новости",
-							"PAGER_SHOW_ALWAYS" => "N",
-							"PAGER_TEMPLATE" => "",
-							"PAGER_DESC_NUMBERING" => "N",
-							"PAGER_SHOW_ALL" => "N",
-						)
-					);?>
-				<!-- -->
-
-<h2>Видео-новости</h2>
-
-<?$APPLICATION->IncludeComponent(
-	"bitrix:player",
-	"",
-	Array(
-		"PLAYER_TYPE" => "auto", 
-		"USE_PLAYLIST" => "N", 
-		"PATH" => "/upload/intro.flv", 
-		"WIDTH" => "400", 
-		"HEIGHT" => "324", 
-		"FULLSCREEN" => "Y", 
-		"SKIN_PATH" => "/bitrix/components/bitrix/player/mediaplayer/skins", 
-		"SKIN" => "bitrix.swf", 
-		"CONTROLBAR" => "bottom", 
-		"WMODE" => "transparent", 
-		"HIDE_MENU" => "N", 
-		"SHOW_CONTROLS" => "Y", 
-		"SHOW_STOP" => "N", 
-		"SHOW_DIGITS" => "Y", 
-		"CONTROLS_BGCOLOR" => "FFFFFF", 
-		"CONTROLS_COLOR" => "000000", 
-		"CONTROLS_OVER_COLOR" => "000000", 
-		"SCREEN_COLOR" => "000000", 
-		"WMODE_WMV" => "window", 
-		"AUTOSTART" => "N", 
-		"REPEAT" => "N", 
-		"VOLUME" => "90", 
-		"DISPLAY_CLICK" => "play", 
-		"MUTE" => "N", 
-		"HIGH_QUALITY" => "Y", 
-		"ADVANCED_MODE_SETTINGS" => "N", 
-		"BUFFER_LENGTH" => "10", 
-		"DOWNLOAD_LINK_TARGET" => "_self" 
-	),
-false
-);?>
-
-<!-- --><!-- -->
+    <!-- slider -->
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="slide__1" style="cursor:pointer;background-image: url('/local/templates/main/images/222.jpg')" onclick="window.location.href='/lechenie-v-kredit'">
+                    <div class="slide__wrap">
+                        <div class="slide__wrapList">
+                            <span class="slide__text" style="font-size: 50px;">Лечите зубы сегодня – платите потом! Лечение в рассрочку и кредит в клиниках «СТОМА»</span>
+                            <span class="osnovnoi_tekst_slaida" style="font-size: 25px;"></span>
+                        </div>
+                        <div class="slide__link"><a href="https://ctoma.ru/lechenie-v-kredit">Узнать подробности</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="slide__1" style="cursor:pointer;background-image: url('/local/templates/main/images/1.jpg')" onclick="window.location.href='/lechenie-v-kredit'">
+                    <div class="slide__wrap">
+                        <div class="slide__wrapList">
+                            <span class="slide__text" style="font-size: 50px;">Успейте пролечиться до повышения цен! Дарим скидку 10%  на терапевтическое лечение до конца февраля!</span>
+                            <span class="osnovnoi_tekst_slaida" style="font-size: 25px;"></span>
+                        </div>
+                        <div class="slide__link"><a href="https://ctoma.ru/akciya/prazdnichnaya-skidka-10-na-terapevticheskoe-lechenie">Узнать подробности</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+    <!-- / slider -->
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/templates/main/header.php
@@ -0,0 +1,243 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<?$APPLICATION->ShowHead()?>
+<title>Стома</title>
+
+<link rel="stylesheet" media="all" href="/local/templates/main/css/style.css">
+
+
+</head>
+
+<body>
+<div id="panel"><?$APPLICATION->ShowPanel();?></div>
+
+
+
+<header class="header" role="banner">
+    <div class="header__top">
+            <div class="header__fixedWrap">
+                <div class="header__fixed width width_norm">
+                    <div class="header__choosingClinic width_paddingLeftRight">
+                            <div id="choosingClinic" xmlns:v-on="http://www.w3.org/1999/xhtml" class="choosingClinic">
+                                <span class="choosingClinic__select">
+                                    <div class="choosingClinic__info">
+                                        <span class="choosingClinic__name">Клиника на Кораблестроителей (м. Приморская)</span>
+                                        <span class="choosingClinic__adress">Кораблестроителей ул., д.32, корпус 3, литер А, 2 этаж</span>
+                                    </div> <span class="choosingClinic__strl"></span>
+                                </span>
+                            </div>
+                    </div>
+                    <noindex>
+                        <div class="header__links width_paddingLeftRight">
+                        <span class="header__link">
+                            <a href="/form/zapros-dokumentov-dla-nalogovogo" class="use-ajax" data-dialog-options="{
+                                    &quot;width&quot;:&quot;600px&quot;,
+                                    &quot;minHeight&quot;:&quot;500px&quot;,
+                                    &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                                    }" data-dialog-type="modal">Налоговый вычет</a>
+                        </span>
+                        <span class="header__link">
+                        <a href="/form/callback" rel="nofollow" class="use-ajax" data-dialog-options="{
+                               &quot;width&quot;:&quot;450px&quot;,
+                               &quot;minHeight&quot;:&quot;500px&quot;,
+                               &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                               }" data-dialog-type="modal">Заказать звонок</a>
+                        </span>
+
+                            <span class="header__link">
+                            <a href="/form/zadat-vopros" class="use-ajax" data-dialog-options="{
+                                    &quot;width&quot;:&quot;500px&quot;,
+                                    &quot;minHeight&quot;:&quot;500px&quot;,
+                                    &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                                    }" data-dialog-type="modal">Задать вопрос</a>
+                        </span>
+                            <span class="header__link">
+                            <a href="/form/kontrol-kacestva" class="use-ajax" data-dialog-options="{
+                               &quot;width&quot;:&quot;500px&quot;,
+                               &quot;minHeight&quot;:&quot;500px&quot;,
+                               &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                               }" data-dialog-type="modal">Контроль качества</a>
+                        </span>
+                        </div>
+                    </noindex>
+                    <noindex>
+                        <div class="header__makeAppointmentWrap width_paddingLeftRight">
+                            <a class="header__makeAppointment" href="/make-an-appointment">Запись на прием</a>
+                        </div>
+                    </noindex>
+                </div>
+            </div>
+            <div class="header__topWrapper width_paddingStandart menuMobile">
+            <div class="header__menuMobile menuMobile__button">
+                <div class="menuMobile__line"></div>
+            </div>
+                <div class="header__menuAndButton menuMobile__content">
+                    <nav class="header__menuTop menuHeader ">
+                    <ul class="menuHeader__ul" role="menu">
+                    <li class="menuHeader__li menuHeader__mobile" role="presentation">
+                    <a class="menuHeader__a" role="menuitem" href="/">Главная</a>
+                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/about" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="taxonomy/term/47">О компании</a>
+                                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/stomatology" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="stomatology">Услуги</a>
+                                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/price" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="taxonomy/term/127">Цены</a>
+                                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/personal" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="personal">Врачи</a>
+                                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/clinics" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="clinics">Клиники</a>
+                                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/portfolio" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="portfolio">Портфолио</a>
+                                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/akcii" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="akcii">Акции</a>
+                                </li>
+                        <li class="menuHeader__li" role="presentation">
+                    <a href="/reviews" class="menuHeader__a" role="menuitem" data-drupal-link-system-path="reviews">Отзывы</a>
+                                </li>
+                  </ul>
+                    </nav>
+                </div>
+
+            <div class="header__logoMobile logo_mobile">
+                <a class="logo__linck" href="/">
+                    <img class="logo__img" src="/local/templates/main/images/logo.jpg" alt="Стоматология в СПб – зубные клиники «СТОМА»" title="«СТОМА» - Сеть стоматологических клиник Санкт-Петербурга">
+                </a>
+            </div>
+
+            <div class="header__searchMobileButton width_paddingStandart">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+
+        </div>
+            <noindex>
+            <div class="header__searchMobile width_paddingLeftRight">
+                <div id="searchM" xmlns:v-bind="http://www.w3.org/1999/xhtml" xmlns:v-on="http://www.w3.org/1999/xhtml" class="search">
+                    <input placeholder="Поиск" class="search__input"> <!---->
+                </div>
+            </div>
+        </noindex>
+        <noindex>
+            <div class="header__buttons width_paddingStandart">
+            <span class="header__link">
+                <a href="/form/callback" class="use-ajax" data-dialog-options="{
+                           &quot;width&quot;:&quot;450px&quot;,
+                           &quot;minHeight&quot;:&quot;500px&quot;,
+                           &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                           }" data-dialog-type="modal">Заказать звонок</a>
+            </span>
+                <span class="header__link">
+                <a href="/form/zadat-vopros" class="use-ajax" data-dialog-options="{
+                           &quot;width&quot;:&quot;500px&quot;,
+                           &quot;minHeight&quot;:&quot;500px&quot;,
+                           &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                           }" data-dialog-type="modal">Задать вопрос</a>
+            </span>
+                <span class="header__link">
+                <a href="/form/kontrol-kacestva" class="use-ajax" data-dialog-options="{
+                           &quot;width&quot;:&quot;500px&quot;,
+                           &quot;minHeight&quot;:&quot;500px&quot;,
+                           &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                           }" data-dialog-type="modal">Контроль качества</a>
+            </span>
+
+                <a class="header__make" href="/make-an-appointment">Запись на прием</a>
+                <span class="header__link" style="flex-basis: 100%">
+                        <a href="/form/zapros-dokumentov-dla-nalogovogo" class="use-ajax" data-dialog-options="{
+                                &quot;width&quot;:&quot;600px&quot;,
+                                &quot;minHeight&quot;:&quot;500px&quot;,
+                                &quot;dialogClass&quot;:&quot;popupDialog&quot;
+                                }" data-dialog-type="modal">Налоговый вычет</a>
+                    </span>
+            </div>
+        </noindex>
+        <div class="header__center">
+            <div class="header__centerWrapper width width_norm width_paddingDisableMobileDevice">
+                <div class="header__logo width_paddingStandart">
+                    <img src="/local/templates/main/images/logo.jpg" class="logo__img" alt="Стоматология в СПб – зубные клиники «СТОМА»" title="«СТОМА» - Сеть стоматологических клиник Санкт-Петербурга">
+                </div>
+                <div class="header__menu">
+                    <div class="glavnoeMenu width_paddingLeftRight">
+                        <ul class="glavnoeMenu__ul" role="menu">
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/stomatology" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="stomatology">Услуги</a>
+                            </li>
+                             <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/price" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="taxonomy/term/127">Цены</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/akcii" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="akcii">Акции</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/clinics" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="clinics">Клиники</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                 <a href="/personal" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="personal">Врачи</a><ul role="menu" class="glavnoeMenu__ul subMenuDoctor"><li class="glavnoeMenu__li"><a href="/personal/stomatologi-terapevty" class="glavnoeMenu__a">Терапевты</a></li><li class="glavnoeMenu__li"><a href="/personal/parodontologi" class="glavnoeMenu__a">Пародонтологи</a></li><li class="glavnoeMenu__li"><a href="/personal/stomatologi-ortopedy" class="glavnoeMenu__a">Ортопеды</a></li><li class="glavnoeMenu__li"><a href="/personal/ortodonty" class="glavnoeMenu__a">Ортодонты</a></li><li class="glavnoeMenu__li"><a href="/personal/stomatologi-hirurgi" class="glavnoeMenu__a">Хирурги</a></li></ul>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/portfolio" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="portfolio">Портфолио</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/about" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="taxonomy/term/47">О компании</a><ul role="menu" class="glavnoeMenu__ul subMenuDoctorN"><li class="glavnoeMenu__li"><a href="/news" class="glavnoeMenu__a">Новости</a></li></ul>
+                            </li>
+                        </ul>
+                </div>
+            </div>
+                <div class="header__search width_paddingStandart">
+                    <noindex>
+                        <div id="search" xmlns:v-bind="http://www.w3.org/1999/xhtml" xmlns:v-on="http://www.w3.org/1999/xhtml" class="search">
+                            <input placeholder="Поиск" class="search__input"> <!---->
+                        </div>
+                    </noindex>
+                </div>
+                <div class="header__info width_paddingStandart">
+                    <div class="views-row">
+                        <div class="views-field views-field-field-phones">
+                            <div class="field field-content call_phone_top"><a href="tel:+78125614086">+7 (812) 561-40-86</a></div>
+                            <div class="grafik">Часы работы call-центра: 09:00-21:00</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="header__menuTablet">
+                <div class="glavnoeMenu width_paddingLeftRight">
+                        <ul class="glavnoeMenu__ul" role="menu">
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/stomatology" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="stomatology">Услуги</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/price" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="taxonomy/term/127">Цены</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/akcii" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="akcii">Акции</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/clinics" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="clinics">Клиники</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/personal" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="personal">Врачи</a><ul role="menu" class="glavnoeMenu__ul subMenuDoctor"><li class="glavnoeMenu__li"><a href="/personal/stomatologi-terapevty" class="glavnoeMenu__a">Терапевты</a></li><li class="glavnoeMenu__li"><a href="/personal/parodontologi" class="glavnoeMenu__a">Пародонтологи</a></li><li class="glavnoeMenu__li"><a href="/personal/stomatologi-ortopedy" class="glavnoeMenu__a">Ортопеды</a></li><li class="glavnoeMenu__li"><a href="/personal/ortodonty" class="glavnoeMenu__a">Ортодонты</a></li><li class="glavnoeMenu__li"><a href="/personal/stomatologi-hirurgi" class="glavnoeMenu__a">Хирурги</a></li></ul>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/portfolio" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="portfolio">Портфолио</a>
+                            </li>
+                            <li class="glavnoeMenu__li" role="presentation">
+                                <a href="/about" class="glavnoeMenu__a" role="menuitem" data-drupal-link-system-path="taxonomy/term/47">О компании</a><ul role="menu" class="glavnoeMenu__ul subMenuDoctorN"><li class="glavnoeMenu__li"><a href="/news" class="glavnoeMenu__a">Новости</a></li></ul>
+                            </li>
+                        </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
