f8f7e3abd6158916beafe539c9fafa2afd6393f8
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-02-14 16:21:16 +0300

    Услуги

---
+++ local/templates/.default/components/bitrix/catalog.section.list/.default/template.php
@@ -0,0 +1,52 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+
+
+?>
+
+
+<div class="uslugi_all width width_full width_paddingStandart">
+    <nav class="uslugi__content width width width_norm width_paddingStandart">
+        <div class="views-element-container">
+            <div class="viewServices">
+                <? foreach ($arResult['SECTIONS'] as &$arSection) {
+                    $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
+                    $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
+                    ?>
+                    <div class="viewServices__row views-row">
+                        <div class="uslugiPrevu">
+                            <a class="uslugiPrevu__link" href="<? echo $arSection['SECTION_PAGE_URL']; ?>">
+                                <img class="uslugiPrevu__img test" src="<? echo $arSection['PICTURE']['SRC']; ?>" alt="<? echo $arSection['NAME']; ?>">
+                                <div class="uslugiPrevu__nameWrap">
+                                <span class="uslugiPrevu__name"><div><? echo $arSection['NAME']; ?></div></span>
+                                </div>
+                                <div class="uslugiPrevu__nameWrapHover">
+                                <span class="uslugiPrevu__nameHover"><div><? echo $arSection['NAME']; ?></div></span>
+                                <span class="uslugiPrevu__moreHover">Узнать</span>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+                <? } ?>
+            </div>
+        </div>
+    </nav>
+</div>
+
+
+
+
+
+
---
+++ local/templates/.default/components/bitrix/catalog/stomatology/realweb/catalog.element/.default/template.php
@@ -0,0 +1,71 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+    die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$name = !empty($arResult['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])
+    ? $arResult['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
+    : $arResult['NAME'];
+
+?>
+
+<h1 class="normPage__title title_grey width width_norm width_paddingStandart">
+    <span class="title__br"></span><span class="title__text">
+        <?= $name; ?>
+    </span><span class="title__br"></span>
+</h1>
+<div class="normPage__container ">
+    <div class="normPage__center width width_norm width_paddingStandart">
+        <div class="normPage__leftSidebar">
+
+            <div class="menuNormPage">
+                <?
+                $elements = \Realweb\Site\Site::getIBlockElements(['IBLOCK_ID' => \Realweb\Site\Site::getIblockId('content'), 'PROPERTY_SHOW_MENU_VALUE' => "да"]);
+                ?>
+
+                <ul class="menuNormPage__list">
+                    <?
+                    foreach ($elements as $el) {
+
+                        ?>
+
+                        <li class="menuNormPage__element">
+                            <a
+                                    class="menuNormPage__linck <? if ($el['CODE'] == $arResult['CODE']) {
+                                        ?>is-active<? }else{ ?>" href="/<?= $el['CODE'] ?>/"<? } ?>"
+                            ><?= $el['NAME'] ?></a>
+                        </li>
+                    <? } ?>
+
+                </ul>
+
+
+            </div>
+
+            <?php \Realweb\Site\Site::showIncludeText('LEFT_BANNER_STATIC'); ?>
+
+        </div>
+        <div class="normPage__content">
+            <div class="block-region-main">
+                <div class="block">
+
+
+                    <div class="normPageDefault content_norm">
+                        <?php echo $arResult['DETAIL_TEXT']; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
