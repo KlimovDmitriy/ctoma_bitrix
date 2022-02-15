f2a5c6c559aedb98e1a8edc22fd6793d9d612d73
Varvara Novchenkova <wassa_17@mail.ru>
2022-02-15 19:07:49 +0300

    Клиники

---
+++ local/templates/.default/components/bitrix/catalog/clinics/realweb/catalog.element/.default/template.php
@@ -0,0 +1,27 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+    die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$name = !empty($arResult['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])
+    ? $arResult['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
+    : $arResult['NAME'];
+
+?>
+
+
+
+<h1 class="service__titleText title_norm width width_norm width_paddingStandart"><? echo $name; ?></h1>
+<div class="serviceDefault content_norm">
+    <?php echo $arResult['DETAIL_TEXT']; ?>
+</div>
---
+++ local/templates/.default/components/bitrix/catalog/clinics/sections.php
@@ -0,0 +1,43 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+
+$this->setFrameMode(true);
+$APPLICATION->IncludeComponent("bitrix:breadcrumb", "", array(
+        "START_FROM" => "0",
+        "PATH" => "",
+        "SITE_ID" => "s1"
+    )
+);
+$APPLICATION->IncludeComponent(
+    "bitrix:catalog.section",
+    "",
+    Array(
+        "ADD_SECTIONS_CHAIN" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "COUNT_ELEMENTS" => "N",
+        "FILTER_NAME" => "sectionsFilter",
+        "IBLOCK_ID" => \Realweb\Site\Site::getIblockId('clinics'),
+        "IBLOCK_TYPE" => "content",
+        "SECTION_CODE" => $_REQUEST["SECTION_CODE"],
+        "SECTION_FIELDS" => array("", ""),
+        "SECTION_ID" => "",
+        "SECTION_URL" => "",
+        "SECTION_USER_FIELDS" => array("", ""),
+        "SHOW_PARENT_NAME" => "Y",
+        "TOP_DEPTH" => "2",
+        "VIEW_MODE" => "LINE"
+    )
+);
