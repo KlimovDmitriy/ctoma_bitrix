31831fefa924a269ea93f08532927d5d366af1d3
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-02-17 12:07:14 +0300

    URL REWRITE FIX

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,27 +1,27 @@
 <?php
-$arUrlRewrite=array (
-  5 =>
-    array (
-        'CONDITION' => '#^/clinics/#',
-        'RULE' => '',
-        'ID' => 'bitrix:catalog',
-        'PATH' => '/clinics/index.php',
-        'SORT' => 100,
-    ),
-  4 =>
-  array (
-    'CONDITION' => '#^/stomatology/#',
-    'RULE' => '',
-    'ID' => 'bitrix:catalog',
-    'PATH' => '/stomatology/index.php',
-    'SORT' => 100,
-  ),
-  3 =>
-  array (
-    'CONDITION' => '#^/#',
-    'RULE' => '',
-    'ID' => NULL,
-    'PATH' => '/pages/index.php',
-    'SORT' => 100,
-  ),
+$arUrlRewrite = array(
+    1 =>
+        array(
+            'CONDITION' => '#^/clinics/#',
+            'RULE' => '',
+            'ID' => 'bitrix:catalog',
+            'PATH' => '/clinics/index.php',
+            'SORT' => 100,
+        ),
+    2 =>
+        array(
+            'CONDITION' => '#^/stomatology/#',
+            'RULE' => '',
+            'ID' => 'bitrix:catalog',
+            'PATH' => '/stomatology/index.php',
+            'SORT' => 100,
+        ),
+    3 =>
+        array(
+            'CONDITION' => '#^/#',
+            'RULE' => '',
+            'ID' => NULL,
+            'PATH' => '/pages/index.php',
+            'SORT' => 100,
+        ),
 );
