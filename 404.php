4c452ff917ac05a137f968d29eed847ad3a8522e
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-02-17 13:04:58 +0300

    404 css fix

--- 404.php
+++ 404.php
@@ -1,26 +1,30 @@
 <?
-include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/urlrewrite.php');
 
 CHTTP::SetStatus("404 Not Found");
-@define("ERROR_404","Y");
+@define("ERROR_404", "Y");
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
-$APPLICATION->SetTitle("404 Not Found");
+$APPLICATION->SetTitle("Страница не найдена | Группа компаний «СТОМА»");
 ?>
+    <main role="main">
+        <div class="content">
+            <div class="page404">
+                <div class="page404__layout">
+                    <div class="page404__text content_norm" style="    padding: 150px 0;">
+                        <h1>Страница не найдена</h1>
+                        <br>
+                        <p>
+                            Запрашиваемая Вами страница была удалена или перемещена.<br> Пожалуйста, воспользуйтесь
+                            поиском, меню или начните с главной страницы.
+                        </p>
 
-<div class="page404">
-    <div class="page404__layout">
-        <div class="page404__text content_norm" style="    padding: 150px 0;">
-            <h1>Страница не найдена</h1>
-            <br>
-            <p>
-                Запрашиваемая Вами страница была удалена или перемещена.<br> Пожалуйста, воспользуйтесь поиском,  меню или начните с главной страницы.
-            </p>
-
-            <a class="button_red" style="color: #fff;" href="/">Перейти на главную</a>
+                        <a class="button_red" style="color: #fff;" href="/">Перейти на главную</a>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+    </main>
 <?php
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
