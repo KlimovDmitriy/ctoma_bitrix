490a870c3e0b18ae4c6d6cf8ab9953ba0cb71648
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-02-17 17:23:33 +0300

    Ошибка в услугах

--- local/templates/.default/components/bitrix/catalog/stomatology/realweb/catalog.element/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/catalog/stomatology/realweb/catalog.element/.default/result_modifier.php
@@ -7,16 +7,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 /** @var array $arResult */
 $arResult['TEXT_PAGE'] = false;
 $arResult['REDIRECT'] = "";
-foreach ($arResult['PROPERTIES']['PAGE_TYPE']['VALUE'] as $PAGE_TYPE) {
-    if ($PAGE_TYPE['TYPE'] == 'PAGE') {
-        $arResult['TEXT_PAGE'] = true;
-    }
-    if ($PAGE_TYPE['TYPE'] == 'EXTERNAL_LINK') {
-        if($PAGE_TYPE['LINK'] != $arResult['DETAIL_PAGE_URL']){
-            $arResult['REDIRECT'] = $PAGE_TYPE['LINK'];
-        }
-    }
-}
+
 
 if ($arResult['PROPERTIES']['GALLERY']['VALUE']) {
     if (Bitrix\Main\Loader::includeModule("iblock")) {
