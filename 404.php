a70a23db9615bff934a4401d6689decbfaa74ad5
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-02-17 12:01:15 +0300

    404 ошибка

--- 404.php
+++ 404.php
@@ -7,6 +7,20 @@ CHTTP::SetStatus("404 Not Found");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("404 Not Found");
+?>
 
+<div class="page404">
+    <div class="page404__layout">
+        <div class="page404__text content_norm" style="    padding: 150px 0;">
+            <h1>Страница не найдена</h1>
+            <br>
+            <p>
+                Запрашиваемая Вами страница была удалена или перемещена.<br> Пожалуйста, воспользуйтесь поиском,  меню или начните с главной страницы.
+            </p>
 
+            <a class="button_red" style="color: #fff;" href="/">Перейти на главную</a>
+        </div>
+    </div>
+</div>
+<?php
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
