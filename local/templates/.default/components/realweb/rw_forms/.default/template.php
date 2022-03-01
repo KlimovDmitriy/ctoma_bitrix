bcf2cd1d24826ddcce80dab2093c1ceebdd68b9c
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-03-01 22:09:26 +0300

    Формы

--- local/templates/.default/components/realweb/rw_forms/.default/template.php
+++ local/templates/.default/components/realweb/rw_forms/.default/template.php
@@ -13,54 +13,52 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<pre>
-
-    <?print_r($arResult['FIELDS']);?>
-</pre>
 <div class="make__form">
     <form class="webform-submission-form webform-submission-add-form webform-submission-makeform-form webform-submission-makeform-add-form webform-submission-makeform-page_variant-make_appointment-panels_variant-0-form webform-submission-makeform-page_variant-make_appointment-panels_variant-0-add-form makeform js-webform-details-toggle webform-details-toggle">
+        <? if (!empty($arResult['FIELDS'])) { ?>
+            <? foreach ($arResult['FIELDS'] as $field) { ?>
 
-        <? foreach ($arResult['FIELDS'] as $field) { ?>
+                <div class="<?= trim($field['WRAP_CSS']); ?>">
 
-            <div class="<?= trim($field['WRAP_CSS']); ?>">
+                    <? switch ($field['TYPE']) {
+                        case 'BUTTON':
+                            ?>
+                            <button type="submit" class="<?= $field['FIELD_CSS']; ?>"><?= $field['TITLE'] ?></button>
 
-                <? switch ($field['TYPE']) {
-                    case 'BUTTON':
-                        ?>
-                        <button type="submit" class="<?= $field['FIELD_CSS']; ?>"><?= $field['TITLE'] ?></button>
+                            <? break; ?>
+                        <? case 'TEXT': ?>
+                            <? if ($field['HIDE_LABEL'] == '') { ?>
+                                <label for="edit-name"
+                                       class=" js-form-required form-required"><?= $field['TITLE'] ?></label>
+                            <? } ?>
+                            <input type="text" id="edit-<?= $field['NAME'] ?>" name="<?= $field['NAME'] ?>"
+                                   placeholder="<?= $field['PLACEHOLDER']; ?>" class="<?= $field['FIELD_CSS']; ?>"
+                                   value=""
+                                   autocomplete="off">
+                            <? break; ?>
 
-                        <? break; ?>
-                    <? case 'TEXT': ?>
-                        <? if ($field['HIDE_LABEL'] == '') { ?>
-                            <label for="edit-name"
-                                   class=" js-form-required form-required"><?= $field['TITLE'] ?></label>
+                        <? case 'CHECKBOX': ?>
+
+
+                            <input type="checkbox" id="edit-agreement" name="<?= $field['NAME'] ?>" value="1"
+                                   checked="checked" class="form-checkbox required" required="required"
+                                   aria-required="true">
+
+                            <? if (!empty($field['DESCRIPTION'])) { ?>
+                                <div class="description">
+                                    <div id="edit-agreement--description"
+                                         class="webform-element-description"><?= $field['DESCRIPTION']; ?></div>
+
+                                </div>
+                            <? } ?>
+
+
+                            <? break; ?>
+
+
                         <? } ?>
-                        <input type="text" id="edit-<?= $field['NAME'] ?>" name="<?= $field['NAME'] ?>"
-                               placeholder="<?= $field['PLACEHOLDER']; ?>" class="<?= $field['FIELD_CSS']; ?>" value=""
-                               autocomplete="off">
-                        <? break; ?>
-
-                    <? case 'CHECKBOX': ?>
-
-
-                        <input type="checkbox" id="edit-agreement" name="<?= $field['NAME'] ?>" value="1"
-                               checked="checked" class="form-checkbox required" required="required"
-                               aria-required="true">
-
-                        <? if (!empty($field['DESCRIPTION'])) { ?>
-                            <div class="description">
-                                <div id="edit-agreement--description"
-                                     class="webform-element-description"><?= $field['DESCRIPTION']; ?></div>
-
-                            </div>
-                        <? } ?>
-
-
-                        <? break; ?>
-
-
-                    <? } ?>
-            </div>
+                </div>
+            <? } ?>
         <? } ?>
     </form>
 
