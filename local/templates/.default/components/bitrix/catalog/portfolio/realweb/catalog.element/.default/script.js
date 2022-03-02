b7bb385726d5eb40050cdc0c3844f144cfb275c4
Varvara Novchenkova <wassa_17@mail.ru>
2022-03-02 14:03:11 +0300

    перенос скрипта twentytwenty

---
+++ local/templates/.default/components/bitrix/catalog/portfolio/realweb/catalog.element/.default/script.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(".twentytwenty-container").twentytwenty({
+        default_offset_pct: 0.5,
+        orientation: 'horizontal',
+        before_label: 'До',
+        after_label: 'После',
+        no_overlay: false,
+        move_slider_on_hover: false,
+        move_with_handle_only: true,
+        click_to_move: false
+    });
+});
--- local/templates/main/js/custom.js
+++ local/templates/main/js/custom.js
@@ -85,16 +85,3 @@ $('.service__bottomMobile .content_toggle').click(function(){
     $('.content_block_service').slideToggle(300);
     return false;
 });
-
-$(function () {
-    $(".twentytwenty-container").twentytwenty({
-        default_offset_pct: 0.5,
-        orientation: 'horizontal',
-        before_label: 'До',
-        after_label: 'После',
-        no_overlay: false,
-        move_slider_on_hover: false,
-        move_with_handle_only: true,
-        click_to_move: false
-    });
-});
