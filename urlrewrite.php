eba3a0f644ef0245e1ad302ee3c97e3fe4afddb8
KlimovDmitriy <59648283+KlimovDmitriy@users.noreply.github.com>
2022-03-02 15:48:49 +0300

    Список врачей

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,43 +1,51 @@
 <?php
-$arUrlRewrite = array(
-    1 =>
-        array(
-            'CONDITION' => '#^/clinics/#',
-            'RULE' => '',
-            'ID' => 'bitrix:catalog',
-            'PATH' => '/clinics/index.php',
-            'SORT' => 100,
-        ),
-    2 =>
-        array(
-            'CONDITION' => '#^/stomatology/#',
-            'RULE' => '',
-            'ID' => 'bitrix:catalog',
-            'PATH' => '/stomatology/index.php',
-            'SORT' => 100,
-        ),
-    3 =>
-        array(
-            'CONDITION' => '#^/reviews/#',
-            'RULE' => '',
-            'ID' => 'bitrix:catalog',
-            'PATH' => '/reviews/index.php',
-            'SORT' => 100,
-        ),
-    4 =>
-        array(
-            'CONDITION' => '#^/portfolio/#',
-            'RULE' => '',
-            'ID' => 'bitrix:catalog',
-            'PATH' => '/portfolio/index.php',
-            'SORT' => 100,
-        ),
-    5 =>
-        array(
-            'CONDITION' => '#^/#',
-            'RULE' => '',
-            'ID' => NULL,
-            'PATH' => '/pages/index.php',
-            'SORT' => 100,
-        ),
+$arUrlRewrite=array (
+  2 => 
+  array (
+    'CONDITION' => '#^/stomatology/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/stomatology/index.php',
+    'SORT' => 100,
+  ),
+  4 => 
+  array (
+    'CONDITION' => '#^/portfolio/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/portfolio/index.php',
+    'SORT' => 100,
+  ),
+  6 => 
+  array (
+    'CONDITION' => '#^/personal/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/personal/index.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/clinics/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/clinics/index.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/reviews/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/reviews/index.php',
+    'SORT' => 100,
+  ),
+  5 => 
+  array (
+    'CONDITION' => '#^/#',
+    'RULE' => '',
+    'ID' => NULL,
+    'PATH' => '/pages/index.php',
+    'SORT' => 100,
+  ),
 );
