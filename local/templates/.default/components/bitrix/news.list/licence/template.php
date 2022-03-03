6e89b2d51e150c3fcee6fe410ebd037fe3ee83b2
Varvara Novchenkova <wassa_17@mail.ru>
2022-03-03 14:33:39 +0300

    лицензии

---
+++ local/templates/.default/components/bitrix/news.list/licence/template.php
@@ -0,0 +1,30 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<div class="width width_norm width_paddingStandart">
+    <div class="node__content">
+        <div class="forSert">
+            <div>
+            <? foreach ($arResult["ITEMS"] as $arItem): ?>
+                <div>
+                    <a href="<?=$arItem["PREVIEW_PICTURE"]["SRC"]; ?>" title="Лицензии и сертификаты" data-fancybox="images" class="colorbox cboxElement">
+                        <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]; ?>" alt="" width="270" height="360">
+                    </a>
+                </div>
+            <? endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
