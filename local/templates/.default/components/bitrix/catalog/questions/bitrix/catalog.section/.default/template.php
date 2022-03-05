a16e2a856c8e6b03a39a61c040e4e45f9205aca4
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-03-05 13:42:57 +0300

    Вопрос-ответ

--- local/templates/.default/components/bitrix/catalog/questions/bitrix/catalog.section/.default/template.php
+++ local/templates/.default/components/bitrix/catalog/questions/bitrix/catalog.section/.default/template.php
@@ -15,47 +15,41 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 $this->setFrameMode(true);
 
 
-$page=1;
-if ($_REQUEST['PAGEN_1']>0){
-    $page=$_REQUEST['PAGEN_1'];
+$page = 1;
+if ($_REQUEST['PAGEN_1'] > 0) {
+    $page = $_REQUEST['PAGEN_1'];
 }
 ?>
 <div class="otzyvy__container">
     <h1 class="otzyvy__title title_grey width width_norm width_paddingStandart">
-        <span class="title__br"></span><span class="title__text">Вопрос-ответ. Страница <?=$page;?> </span><span class="title__br"></span>
+        <span class="title__br"></span><span class="title__text">Вопрос-ответ. Страница <?= $page; ?> </span><span
+                class="title__br"></span>
     </h1>
     <div class="faq_link">
         <a href="/faq/" class="button_red">Ответы на часто задаваемые вопросы</a>
     </div>
-    <div class="otzyvy__content width width_norm width_paddingStandart">
+    <div class="questions__content width width_norm width_paddingStandart">
         <div class="views-element-container">
             <div>
                 <? foreach ($arResult['ITEMS'] as $item) { ?>
 
                     <div class="views-row">
-                        <div class="otzyvDefault width width_light">
-                            <span class="otzyvDefault__podlojka"></span>
-                            <div class="questionTeaser__content ">
+                        <div class="questionTeaser">
+                            <div class="questionTeaser__content width width_light">
 
-                                <div class="otzyvDefault__top">
+                                <div class="questionTeaser__question content_norm ">
 
-
-                                    <div class="otzyvDefault__info width_paddingStandart">
-
-                                        <div class="otzyvDefault__body">
-                                            Вопрос:
-                                            <div>
-                                                <?= html_entity_decode($item['PREVIEW_TEXT']); ?>
-                                            </div>
-
-                                        </div>
+                                    Вопрос:
+                                    <div>
+                                        <?= html_entity_decode($item['PREVIEW_TEXT']); ?>
                                     </div>
-                                </div>
 
-                                <div class="questionTeaser__more"><a class="button_red"
-                                                                   href="<?= $item['DETAIL_PAGE_URL']; ?>/">Посмотреть ответ</a>
-                                </div>
+                                    <div class="questionTeaser__more"><a class="button_red"
+                                                                         href="<?= $item['DETAIL_PAGE_URL']; ?>/">Посмотреть
+                                            ответ</a>
+                                    </div>
 
+                                </div>
                             </div>
                         </div>
                     </div>
--- questions/.section.php
+++ questions/.section.php
@@ -1,3 +1,3 @@
 <?
-$sSectionName = "Отзывы";
+$sSectionName = "Вопросы - Ответы";
 ?>
