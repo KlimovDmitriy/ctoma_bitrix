d663e3ebbdc73c2808dac3e0b344dd3b0c114faa
KlimovDmitriy <59648283+KlimovDmitriy@users.noreply.github.com>
2022-03-10 13:00:49 +0300

    Листинг новостей

--- local/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
@@ -5,24 +5,24 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 }
 CModule::IncludeModule("iblock");
 
-foreach ($arResult["ITEMS"] as $key => $arElement) {
-    if (!empty($arElement["DETAIL_PICTURE"]["SRC"])) {
-        $arFileTmp = CFile::ResizeImageGet(
-          $arElement["DETAIL_PICTURE"],
-          array("width" => 320, "height" => 320),
-          BX_RESIZE_IMAGE_PROPORTIONAL_ALT ,
-          true,
-          false,
-          false,
-          99
-        );
-        $arResult["ITEMS"][$key]["DETAIL_PICTURE"] = array(
-          "SRC" => $arFileTmp["src"],
-          "WIDTH" => $arFileTmp["width"],
-          "HEIGHT" => $arFileTmp["height"],
-        );
-    }
-}
+//foreach ($arResult["ITEMS"] as $key => $arElement) {
+//    if (!empty($arElement["DETAIL_PICTURE"]["SRC"])) {
+//        $arFileTmp = CFile::ResizeImageGet(
+//          $arElement["DETAIL_PICTURE"],
+//          array("width" => 320, "height" => 320),
+//          BX_RESIZE_IMAGE_PROPORTIONAL_ALT ,
+//          true,
+//          false,
+//          false,
+//          99
+//        );
+//        $arResult["ITEMS"][$key]["DETAIL_PICTURE"] = array(
+//          "SRC" => $arFileTmp["src"],
+//          "WIDTH" => $arFileTmp["width"],
+//          "HEIGHT" => $arFileTmp["height"],
+//        );
+//    }
+//}
 foreach ($arResult['ITEMS'] as $id => $news) {
     $text = preg_replace('/(<[^>]*) style=("[^"]+"|\'[^\']+\')([^>]*>)/i', '$1$3', $news['~DETAIL_TEXT']);
     $text = preg_replace('#(<img(.*?)>)#', '', $text);
