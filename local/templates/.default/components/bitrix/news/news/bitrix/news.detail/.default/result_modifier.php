c89baf0621e52a24cc76e5545e3f21256e14247e
KlimovDmitriy <59648283+KlimovDmitriy@users.noreply.github.com>
2022-03-10 14:02:21 +0300

    Детальные новости

--- local/templates/.default/components/bitrix/news/news/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/news/news/bitrix/news.detail/.default/result_modifier.php
@@ -19,13 +19,3 @@ CModule::IncludeModule("iblock");
 //        );
 //
 //    }
-
-
-foreach ($arResult['PROPERTIES']['WORK_PLACE']['VALUE'] as $clinic) {
-    $clinicId[] = $clinic;
-}
-$clinicsList = CIBlockElement::GetList(['ID'=>'ASC'], ['ID' => $clinicId], false, false, ['ID', 'IBLOCK_ID', 'NAME', 'DETAIL_PAGE_URL', 'LIST_PAGE_URL']);
-
-while ($clinic = $clinicsList->GetNext()) {
-    $arResult['CLINICS'][] = $clinic;
-}
