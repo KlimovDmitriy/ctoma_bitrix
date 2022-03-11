cf93e0c5d939594f18dd0454274da10739e0e110
KlimovDmitriy <59648283+KlimovDmitriy@users.noreply.github.com>
2022-03-11 18:29:23 +0300

    Исправление ошибок отображения листинга врачей и детальной страницы врачей

--- local/templates/.default/components/bitrix/news/vraci/bitrix/news.list/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/news/vraci/bitrix/news.list/.default/result_modifier.php
@@ -24,6 +24,15 @@ foreach ($arResult["ITEMS"] as $key => $arElement) {
     }
 }
 foreach ($arResult['ITEMS'] as $id => $doctor) {
+    $clinicId = [];
     $reviewsList = CIBlockElement::GetList(['SORT'=>'ASC'], ['PROPERTY_DOCTOR'=>$doctor['ID'], 'ACTIVE'=>'Y'], false, false, ['ID', 'IBLOCK_ID']);
     $arResult['ITEMS'][$id]['REVIEWS_COUNT'] = $reviewsList->SelectedRowsCount();
+    foreach ($arResult['ITEMS'][$id]['PROPERTIES']['WORK_PLACE']['VALUE'] as $clinic) {
+        $clinicId[] = $clinic;
+    }
+    $clinicsList = CIBlockElement::GetList(['ID'=>'ASC'], ['ID' => $clinicId], false, false, ['ID', 'IBLOCK_ID', 'NAME', 'DETAIL_PAGE_URL', 'LIST_PAGE_URL']);
+
+    while ($clinic = $clinicsList->GetNext()) {
+        $arResult['ITEMS'][$id]['CLINICS'][] = $clinic;
+    }
 }
