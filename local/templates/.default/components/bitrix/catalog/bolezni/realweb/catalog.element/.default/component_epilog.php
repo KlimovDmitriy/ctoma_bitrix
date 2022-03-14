434a369c4c41c5307fb3e9e3c9d71de8b6801e15
Aleksandr Aleksenko <sanya_dream@mail.ru>
2022-03-14 17:24:01 +0300

    Метатеги болезни

--- local/templates/.default/components/bitrix/catalog/bolezni/realweb/catalog.element/.default/component_epilog.php
+++
@@ -1,7 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-
-$APPLICATION->SetPageProperty("title", 'Вопрос #'.$arResult['DISPLAY_ID'].' от '.FormatDateFromDB($arResult["DATE_ACTIVE_FROM"], 'SHORT').' - вопрос-ответ | Группа компаний «СТОМА»');
-$APPLICATION->SetPageProperty("description", 'Вопрос #'.$arResult['DISPLAY_ID'].' от посетителя, оставленный '.FormatDateFromDB($arResult["DATE_ACTIVE_FROM"], 'SHORT').' в разделе «вопрос-ответ» стоматологии «СТОМА»');
-
-?>
