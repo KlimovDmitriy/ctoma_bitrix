58dd9ab03e1c5d70e31efb0a4627f160dfb05658
Varvara Novchenkova <wassa_17@mail.ru>
2022-03-15 12:59:36 +0300

    примеры работ в услугах

---
+++ local/templates/.default/components/bitrix/catalog/stomatology/bitrix/catalog.section/other/template.php
@@ -0,0 +1,34 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
+
+<div class="views-element-container">
+    <h2 class="portfolioBlock__title">
+        <span class="title__br"></span>
+        <span class="title__text">Другие работы</span>
+        <span class="title__br"></span>
+    </h2>
+    <div class="uslugi_all width width_full width_paddingStandart">
+        <div class="uslugi__content width width width_norm width_paddingStandart">
+            <div class="views-element-container">
+                <div class="viewServices portfolioBlocks">
+                    <?foreach($arResult["ITEMS"] as $arElement):?>
+                    <div class="views-row">
+                        <div class="uslugiPrevu portfolioPreview">
+                            <a class="uslugiPrevu__link" href="/portfolio/<?=$arElement["CODE"]?>/">
+                                <div class="portfolioImgFix"> <img src="<?=$arElement['PHOTO_AFTER'];?>" alt=""></div>
+                                <div class="uslugiPrevu__nameWrap">
+                                    <span class="uslugiPrevu__name"><?=$arElement["NAME"]?></span>
+                                </div>
+                                <div class="uslugiPrevu__nameWrapHover">
+                                    <span class="uslugiPrevu__nameHover"><?=$arElement["NAME"]?></span>
+                                    <span class="uslugiPrevu__moreHover">Узнать</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <?endforeach;?>
+                </div>
+            </div>
+            <div class="all__work__examples_wr"><a class="actions__zapis" href="/portfolio/">Все примеры</a></div>
+        </div>
+    </div>
+</div>
