9da98df7c6822ecc7a90aa91c495f4efa4dd72ca
KlimovDmitriy <59648283+KlimovDmitriy@users.noreply.github.com>
2022-03-15 14:18:14 +0300

    Врачи на странице клиники

--- local/templates/.default/components/bitrix/catalog/clinics/realweb/catalog.element/.default/template.php
+++ local/templates/.default/components/bitrix/catalog/clinics/realweb/catalog.element/.default/template.php
@@ -14,8 +14,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 global $clinic;
-global $clinicFilter;
-$clinicFilter = ['PROPERTY_WORK_PLACE'=>$arResult["ID"]];
+$GLOBALS["clinicFilter"]["PROPERTY_WORK_PLACE"] = $arResult['ID'];
 $clinic = [
         'NAME' => $arResult['NAME'],
     'LONGITUDE' => $arResult['PROPERTIES']['LONGITUDE'],
