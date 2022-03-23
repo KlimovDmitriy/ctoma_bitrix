615d13f9d130c5530f9c294fbf6d25ca05c0e33e
Varvara Novchenkova <wassa_17@mail.ru>
2022-03-23 15:16:06 +0300

    мета теги на дет. врача

---
+++ local/templates/.default/components/bitrix/news/vraci/bitrix/news.detail/.default/component_epilog.php
@@ -0,0 +1,7 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$APPLICATION->SetPageProperty("title", $arResult['NAME'].': врач '.$arResult['DOCTOR_POSITION'].'  в клинике «Стома», отзывы пациентов');
+$APPLICATION->SetPageProperty("description", 'Врач стоматологической клиники «СТОМА» '.$arResult['NAME'].'. Должность, образование и достижения специалиста. Отзывы и онлай-запись на прием.');
+
+
+?>
