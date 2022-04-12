159bcdf9a05d7ef9bbfe623cc7ac3ffa1c4ec67e
Алексенко Александр <aleksenko@racurs.agency>
2022-04-12 17:10:47 +0300

    301 from uppercase

--- local/modules/realweb.site/lib/site.php
+++ local/modules/realweb.site/lib/site.php
@@ -32,6 +32,15 @@ class Site
                 define($CONSTANT, $row['ID']);
             }
         }
+        Global $APPLICATION;
+        $current_url = $APPLICATION->GetCurDir();
+
+        if ( $_SERVER['REQUEST_URI'] != strtolower( $_SERVER['REQUEST_URI']) && $current_url != strtolower( $current_url) ) {
+            $new_url = str_replace($current_url,'',$_SERVER['REQUEST_URI']);
+            header('Location: https://'.$_SERVER['HTTP_HOST'] .
+                strtolower($current_url) . $new_url, true, 301);
+            exit();
+        }
 
     }
 
