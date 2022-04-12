2e19cfe64a93f7846ecda8150f442438c7fc63ad
Алексенко Александр <aleksenko@racurs.agency>
2022-04-12 17:07:47 +0300

    301 from uppercase remove

--- local/modules/realweb.site/include.php
+++ local/modules/realweb.site/include.php
@@ -8,13 +8,5 @@ AddEventHandler("main", "OnEndBufferContent", array('\Realweb\Site\Webp', 'conve
 function onPageStart()
 {
     Site::definders();
-    Global $APPLICATION;
-    $current_url = $APPLICATION->GetCurDir();
-    if ( $_SERVER['REQUEST_URI'] != strtolower( $_SERVER['REQUEST_URI']) && $current_url != strtolower( $current_url) ) {
-        $new_url = str_replace($current_url,'',$_SERVER['REQUEST_URI']);
-        header('Location: https://'.$_SERVER['HTTP_HOST'] .
-            strtolower($current_url) . $new_url, true, 301);
-        exit();
-    }
     //$GLOBALS['arrFilterMainBanner']['PROPERTY_TYPE'] = current(Site::getPropEnumValues(array('IBLOCK_ID' => IBLOCK_CONTENT_MAIN_BANNER, 'XML_ID' => 'main', 'CODE' => 'TYPE')))['ID'];
 }
