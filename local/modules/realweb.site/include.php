a549a304e32491f8f0af79f92cd62a9e79664a7a
aleksenko@racurs.agency <aleksenko@racurs.agency>
2022-04-19 14:07:48 +0300

    DDOS

--- local/modules/realweb.site/include.php
+++ local/modules/realweb.site/include.php
@@ -1,4 +1,5 @@
 <?php
+
 use \Realweb\Site\Site;
 
 AddEventHandler("main", "OnPageStart", "onPageStart");
@@ -6,14 +7,24 @@ AddEventHandler("main", "OnBeforeProlog", "onProlog");
 AddEventHandler('iblock', 'OnIBlockPropertyBuildList', array('Realweb\Site\Property\PageType', 'GetUserTypeDescription'));
 AddEventHandler('iblock', 'OnIBlockPropertyBuildList', array('Realweb\Site\Property\YoutubeVideo', 'GetUserTypeDescription'));
 AddEventHandler("main", "OnEndBufferContent", array('\Realweb\Site\Webp', 'convertAllToWebp'));
-AddEventHandler("iblock", "OnAfterIBlockElementAdd", Array("\Realweb\Site\Handlers", "OnAfterIBlockElementAddHandler"));
+AddEventHandler("iblock", "OnAfterIBlockElementAdd", array("\Realweb\Site\Handlers", "OnAfterIBlockElementAddHandler"));
 function onPageStart()
 {
     Site::definders();
     //$GLOBALS['arrFilterMainBanner']['PROPERTY_TYPE'] = current(Site::getPropEnumValues(array('IBLOCK_ID' => IBLOCK_CONTENT_MAIN_BANNER, 'XML_ID' => 'main', 'CODE' => 'TYPE')))['ID'];
 }
-function onProlog(){
-    if (!in_array($_SESSION['SESS_COUNTRY_ID'], array('RU', 'BY', 'KZ'))) {
-        die('This site temporary unavailable.');
+
+function onProlog()
+{
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $ipInfo = json_decode(file_get_contents("http://ip-api.com/json/{$ip}?fields=status,message,country,countryCode"), true);
+
+    if (isset($ipInfo['country']) && $ipInfo['country'] != 'Russia') {   //print('BLOCKED COUNTRY');
+
+        http_response_code(403);
+
+        die('Forbidden');
     }
+
 }
