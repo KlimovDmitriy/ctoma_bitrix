20c9791bf74dc9ade773d721e70804c82fbe037d
aleksenko@racurs.agency <aleksenko@racurs.agency>
2022-04-19 18:53:59 +0300

    404 page

--- local/modules/realweb.site/include.php
+++ local/modules/realweb.site/include.php
@@ -1,13 +1,27 @@
 <?php
+
 use \Realweb\Site\Site;
 
 AddEventHandler("main", "OnPageStart", "onPageStart");
 AddEventHandler('iblock', 'OnIBlockPropertyBuildList', array('Realweb\Site\Property\PageType', 'GetUserTypeDescription'));
 AddEventHandler('iblock', 'OnIBlockPropertyBuildList', array('Realweb\Site\Property\YoutubeVideo', 'GetUserTypeDescription'));
 AddEventHandler("main", "OnEndBufferContent", array('\Realweb\Site\Webp', 'convertAllToWebp'));
-AddEventHandler("iblock", "OnAfterIBlockElementAdd", Array("\Realweb\Site\Handlers", "OnAfterIBlockElementAddHandler"));
+AddEventHandler("iblock", "OnAfterIBlockElementAdd", array("\Realweb\Site\Handlers", "OnAfterIBlockElementAddHandler"));
+AddEventHandler("main", "OnEpilog", "OnEpilog");
+
+
 function onPageStart()
 {
     Site::definders();
     //$GLOBALS['arrFilterMainBanner']['PROPERTY_TYPE'] = current(Site::getPropEnumValues(array('IBLOCK_ID' => IBLOCK_CONTENT_MAIN_BANNER, 'XML_ID' => 'main', 'CODE' => 'TYPE')))['ID'];
+}
+
+function OnEpilog()
+{
+    /*Показать 404 для всего где есть ?page*/
+    if (strpos($_SERVER['REQUEST_URI'], '?page') != false) {
+
+        \Bitrix\Iblock\Component\Tools::process404("", true, true, true, "");
+    }
+
 }
