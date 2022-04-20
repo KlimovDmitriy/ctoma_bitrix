d5eed96dca4a67ee365d0f7784e3645d47660370
Алексенко Александр <aleksenko@racurs.agency>
2022-04-20 14:37:06 +0300

    Мелкие правки шаблона

--- local/templates/.default/components/bitrix/news.list/reviews_main/script.js
+++ local/templates/.default/components/bitrix/news.list/reviews_main/script.js
@@ -1,10 +1,12 @@
-var reviewsMain = new Swiper('.reviews__swiper', {
-    navigation: {
-        nextEl: '.reviews__swiperButtonNext',
-        prevEl: '.reviews__swiperButtonPrev',
-    },
-    paginationClickable: true,
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true
-});
+$(document).ready(function(){
+    var reviewsMain = new Swiper('.reviews__swiper', {
+        navigation: {
+            nextEl: '.reviews__swiperButtonNext',
+            prevEl: '.reviews__swiperButtonPrev',
+        },
+        paginationClickable: true,
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true
+    });
+})
