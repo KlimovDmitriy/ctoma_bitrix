461a338e222de9d8bd66a3cbb5a3b1ea8e3f8392
Алексенко Александр <aleksenko@racurs.agency>
2022-04-20 13:16:04 +0300

    Редиректы + 404 старой пагинации - отзывы

--- reviews/index.php
+++ reviews/index.php
@@ -10,7 +10,23 @@ if (strpos($_SERVER['REQUEST_URI'], 'page') != false) {
 
     \CHTTP::setStatus("404 Not Found");
     global $APPLICATION;
-
+echo '<main role="main">
+<div class="content">
+<div class="page404">
+<div class="page404__layout">
+<div class="page404__text content_norm" style="    padding: 150px 0;">
+<h1>Страница не найдена</h1>
+<br>
+<p>
+Запрашиваемая Вами страница была удалена или перемещена.<br> Пожалуйста, воспользуйтесь
+поиском, меню или начните с главной страницы.
+</p>
+<a class="button_red" style="color: #fff;" href="/">Перейти на главную</a>
+</div>
+</div>
+</div>
+</div>
+</main>';
     return '';
 }
 
