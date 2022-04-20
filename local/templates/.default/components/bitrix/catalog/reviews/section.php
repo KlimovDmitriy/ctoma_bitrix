981e7dfdb6dcb1e8523bbc131c43ada4f58e560e
Алексенко Александр <aleksenko@racurs.agency>
2022-04-20 13:17:57 +0300

    Редиректы + 404 старой пагинации - отзывы

--- local/templates/.default/components/bitrix/catalog/reviews/section.php
+++ local/templates/.default/components/bitrix/catalog/reviews/section.php
@@ -9,7 +9,32 @@ if ($_REQUEST['PAGEN_1'] > 0) {
     $desc .= ' - страница ' . (int)$_REQUEST['PAGEN_1'];
 }
 
+/*Показать 404 для всего где есть ?page*/
+if (strpos($_SERVER['REQUEST_URI'], 'page') != false) {
+    define("ERROR_404", "Y");
 
+    \CHTTP::setStatus("404 Not Found");
+    global $APPLICATION;
+    echo '<main role="main">
+<div class="content">
+<div class="page404">
+<div class="page404__layout">
+<div class="page404__text content_norm" style="    padding: 150px 0;">
+<h1>Страница не найдена</h1>
+<br>
+<p>
+Запрашиваемая Вами страница была удалена или перемещена.<br> Пожалуйста, воспользуйтесь
+поиском, меню или начните с главной страницы.
+</p>
+<a class="button_red" style="color: #fff;" href="/">Перейти на главную</a>
+</div>
+</div>
+</div>
+</div>
+</main>';
+    require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
+    return '';
+}
 
 $APPLICATION->SetTitle($title);
 
