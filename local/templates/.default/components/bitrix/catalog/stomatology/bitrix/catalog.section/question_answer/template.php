9ea1cf1a76c0a1238e5495e4da5838a9b9ecabb7
Novchenkova <novchenkova@racurs.agency>
2022-05-27 17:13:17 +0300

    микроразметка вопрос-ответ

--- local/templates/.default/components/bitrix/catalog/stomatology/bitrix/catalog.section/question_answer/template.php
+++ local/templates/.default/components/bitrix/catalog/stomatology/bitrix/catalog.section/question_answer/template.php
@@ -1,22 +1,22 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
 
 <? if (count($arResult['ITEMS']) > 0) { ?>
-    <div class="faq-blk content_norm">
+    <div class="faq-blk content_norm" itemscope itemtype="https://schema.org/FAQPage">
         <div class="views-element-container">
             <div>
                 <header>
                     <h2>Вопрос-ответ</h2>
                 </header>
                     <?foreach($arResult["ITEMS"] as $arElement):?>
-                    <div class="views-row">
+                    <div class="views-row" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                         <div class="views-field views-field-field-vopros-faq">
-                            <div class="field-content">
+                            <div class="field-content" itemprop="name">
                                 <?=htmlspecialchars_decode($arElement["PREVIEW_TEXT"]);?>
                             </div>
                         </div>
                         <div class="views-field views-field-field-otvet-faq">
-                            <div class="field-content">
-                                <?=htmlspecialchars_decode($arElement["DETAIL_TEXT"]);?>
+                            <div class="field-content" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                                <div itemprop="text"><?=htmlspecialchars_decode($arElement["DETAIL_TEXT"]);?></div>
                             </div>
                         </div>
                     </div>
