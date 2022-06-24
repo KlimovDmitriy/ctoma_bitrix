872110201520c38ec09d36fc75529e1adfb03df4
Алексенко Александр <aleksenko@racurs.agency>
2022-06-24 17:51:18 +0300

    Фиксированный номер до подмены

--- local/templates/.default/components/realweb/blank/clinics/template.php
+++ local/templates/.default/components/realweb/blank/clinics/template.php
@@ -15,7 +15,7 @@
             <a href="/clinics/<?= $clinic['CODE'] ?>"><span
                         class="choosingClinic__listName"><?= $clinic['NAME'] ?></span>
                 <span class="choosingClinic__minAdress"><?= $clinic['PROPERTIES']['ADDRESS']['VALUE'] ?></span>
-                <span class="choosingClinic__phone <?php if ($clinic['PROPERTIES']['CALLTOUCH']['VALUE']): ?><?= $clinic['PROPERTIES']['CALLTOUCH']['VALUE'] ?><?php endif; ?>"><?= $clinic['PROPERTIES']['PHONES']['VALUE'][0] ?></span></a><?if (!empty($clinic['PROPERTIES']['ADDS']['VALUE'][0])){?> (доб. <?= $clinic['PROPERTIES']['ADDS']['VALUE'][0] ?>)<?}?></div>
+                <span class="choosingClinic__phone <?php if ($clinic['PROPERTIES']['CALLTOUCH']['VALUE']): ?><?= $clinic['PROPERTIES']['CALLTOUCH']['VALUE'] ?><?php endif; ?>">+7 (812) 324-01-80<?//= $clinic['PROPERTIES']['PHONES']['VALUE'][0] ?></span></a><?if (!empty($clinic['PROPERTIES']['ADDS']['VALUE'][0])){?> (доб. <?= $clinic['PROPERTIES']['ADDS']['VALUE'][0] ?>)<?}?></div>
 
 
         <? } ?>
